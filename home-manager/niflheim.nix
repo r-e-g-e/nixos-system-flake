7bf41391fc3a3535f6ffb4087277f4390b3436e8
Rodrigo Tavares <rodrigo.tavares.lima@hotmail.com>
2024-02-03 08:26:45 -0300

    config files

--- home-manager/niflheim.nix
+++ home-manager/niflheim.nix
@@ -53,62 +53,18 @@ in  {
       discord
     ];
     file = {
-      # ".config/neofetch/config.conf".source = "./configs/neofetch.conf";
-      ".config/gtk-2.0/config.ini".text = gtkConfig;
-      ".config/gtk-3.0/config.ini".text = gtkConfig;
-      ".config/gtk-4.0/config.ini".text = gtkConfig;
-      ".config/hypr/hyprland.conf".source = "./configs/hyprland.conf";
-      # ".config/dunst/dunstrc".text = ''
-      # [global]
-      #     font = JetbrainsMono NF 11
-      #     word_wrap = yes
-      #     markup = full
-      #     follow = mouse
-      #     offset = 20x24
-
-      #     width = (0, 500)
-      #     corner_radius = 10
-
-      #     timeout = 5
-      #     show_age_threshold = 60
-      #     stack_duplicates = true
-      #     hide_duplicate_count = false
-      #     show_indicators = no
-      #     indicate_hidden = yes
-
-      #     frame_width = 2
-      #     progress_bar_frame_width = 0
-      #     progress_bar_corner_radius = 5
-
-      #     min_icon_size = 0
-      #     max_icon_size = 80
-      #     icon_corner_radius = 5
-      #     text_icon_padding = 10
-
-
-      #     mouse_left_click = do_action
-      #     mouse_middle_click = close_all
-      #     mouse_right_click = close_current
-
-      # [urgency_low]
-      #     background = "#!!{primary}88"
-      #     foreground = "#!!text"
-      #     frame_color = "#!!accent"
-
-      # [urgency_normal]
-      #     background = "#!!{primary}88"
-      #     foreground = "#!!text"
-      #     frame_color = "#!!accent"
-
-      # [urgency_critical]
-      #     background = "#!!{primary}88"
-      #     foreground = "#!!text"
-      #     frame_color = "#!!accent"
-      # '';
+      "~/.config/neofetch/config.conf".source = ./configs/neofetch.conf;
+      "~/.config/hypr/hyprland.conf".source = ./configs/hyprland.conf;
+      "~/.config/gtk-2.0/config.ini".text = gtkConfig;
+      "~/.config/gtk-3.0/config.ini".text = gtkConfig;
+      "~/.config/gtk-4.0/config.ini".text = gtkConfig;
     };
   };
 
-  services.dunst.enable = true;
+  services.dunst = {
+    enable = true;
+    configFile = ./configs/dunstrc;
+  };
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium-fhs;
