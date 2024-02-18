ecc63b5f83d9bdad807915ca3792b1d70836f085
Rodrigo Tavares <rodrigo.tavares.lima@hotmail.com>
2024-02-18 17:26:47 -0300

    nix.nix common file

---
+++ hosts/common/nix.nix
@@ -0,0 +1,45 @@
+{inputs, ...}: {
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # If you want to use overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    config.allowUnfree = true;
+  };
+
+  nix = {
+    # This will add each flake input as a registry
+    # To make nix3 commands consistent with your flake
+    registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+
+    # This will additionally add your inputs to the system's legacy channels
+    # Making legacy nix commands consistent as well, awesome!
+    nixPath = ["/etc/nix/path"];
+    settings = {
+      # Nix binary cache URLs
+      substituters = [ "https://cache.nixos.org/" "https://hyprland.cachix.org/"];
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ];
+
+      # Enable flakes and new 'nix' command
+      experimental-features = ["nix-command" "flakes"];
+      # Deduplicate and optimize nix store
+      auto-optimise-store = true;
+    };
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+  };
+}
