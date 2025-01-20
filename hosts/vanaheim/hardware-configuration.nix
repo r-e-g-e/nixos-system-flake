8c25e5242f14e0bb233d6e39a8e89d8bcb1d50e5
Rodrigo Tavares <rodrigo@noxis.com.br>
2025-01-20 13:40:06 -0300

    swapfile

--- hosts/vanaheim/hardware-configuration.nix
+++ hosts/vanaheim/hardware-configuration.nix
@@ -33,14 +33,23 @@
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  swapDevices = [ ];
+  swapDevices = [
+    {
+      device = /media/swapfile;
+      size = 8*1024;
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.br-bcdc2ed95f03.useDHCP = lib.mkDefault true;
+  # networking.interfaces.docker0.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.veth8ab0197.useDHCP = lib.mkDefault true;
+  # networking.interfaces.vethe769867.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
